c72342e0b1e347f9f31d6aaeca14225d1cd6167f
pwfrank <frank.p.wiegand@gmail.com>
2019-04-25 19:23:29 -0400

    Tweak letter kerning and update script

--- update.sh
+++ update.sh
@@ -5,7 +5,6 @@ makepkg -si --noconfirm
 cd src/st-0.8.2/
 rm -rf *.o* LEGACY FAQ TODO config.def.h st
 cd ../
-rm -f *.diff *.h *.desktop
+rm -f *.diff *.h *.desktop *.gz
 cd ../
-rsync  src/st-0.8.2/* ../st-pub/
 rm -rf pkg *.xz *.gz
